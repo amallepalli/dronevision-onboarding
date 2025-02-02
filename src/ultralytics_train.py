2209c701d66600f4744dc08ff78a1df46f4c32fe
amallepalli <adityamallepalli@gmail.com>
2025-02-02 17:57:49 -0500

    Finished Task 1

--- src/ultralytics_train.py
+++ src/ultralytics_train.py
@@ -1,7 +1,8 @@
-from ultralytics import YOLO
+if __name__ == "__main__":  
+    from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+    # Load a model
+    model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
-# Train the model
-results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
+    # Train the model
+    results = model.train(data="VisDrone.yaml", epochs=5, imgsz=640,)
